abe927692251297127500f6888b23982e3082e57
tinmux <tin@termux.j6>
2019-09-23 21:07:29 +0200

    Dotfile : install check of file exists

--- dotfile/install.sh
+++ dotfile/install.sh
@@ -1,22 +1,28 @@
+function try_link {
+    [ -f $2 ] \
+        && echo "FF : $2 exists" \
+        || ( ln -s $1 $2 && echo "OK : $2 created" )
+}
+
 # Git
-ln -s ~/.vim/dotfile/gitconfig  ~/.gitconfig
+try_link ~/.vim/dotfile/gitconfig  ~/.gitconfig
 
 # Tmux
-ln -s ~/.vim/dotfile/tmux.conf  ~/.tmux.conf  # Tmux
+try_link ~/.vim/dotfile/tmux.conf  ~/.tmux.conf
 
 # Termux
-mkdir ~/.termux
-ln -s ~/.vim/dotfile/termux.properties ~/.termux/termux.properties
+[ -d ~/.termux ] || mkdir ~/.termux
+try_link ~/.vim/dotfile/termux.properties ~/.termux/termux.properties
 
 # Vim
-ln -s ~/.vim/dotfile/vimrc ~/.vimrc           # Vim
+try_link ~/.vim/dotfile/vimrc ~/.vimrc
 
 # Perl
-ln -s ~/.vim/dotfile/replyrc ~/.replyrc       # Reply (iperl) (load perlrc)
-ln -s ~/.vim/dotfile/perlrc.pl ~/.perlrc.pl   # Perl (run command)
-ln -s ~/.vim/dotfile/Tool.pm ~/Software/Perl/Lib/Tool.pm
+try_link ~/.vim/dotfile/replyrc ~/.replyrc
+try_link ~/.vim/dotfile/perlrc.pl ~/.perlrc.pl
+try_link ~/.vim/dotfile/Tool.pm ~/Software/Perl/Lib/Tool.pm
 
 # Bash
-ln -s ~/.vim/dotfile/bash_aliases.sh ~/.bash_aliases.sh
-ln -s ~/.vim/dotfile/bashrc ~/.bashrc
-ln -s ~/.vim/dotfile/inputrc ~/.inputrc
+try_link ~/.vim/dotfile/bash_aliases.sh ~/.bash_aliases.sh
+try_link ~/.vim/dotfile/bashrc ~/.bashrc
+try_link ~/.vim/dotfile/inputrc ~/.inputrc
