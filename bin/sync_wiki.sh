b6584778d658030807ea59be7638ae552634842c
tinmarino <tinmarino@gmail.com>
2019-10-08 22:29:19 +0200

    ___ <- sync_git.sh

--- bin/sync_wiki.sh
+++ bin/sync_wiki.sh
@@ -6,24 +6,34 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 function ssync(){
     title=$(basename -- "$1")
     title=${title^^}
+    let ret=0
     echo "--->  $title  ================================================="
     mkdir $1 2> /dev/null && echo "Created directory $1"
     pushd $1 > /dev/null
     git add .
     git commit -m "___ <- sync_git.sh"
     git pull --rebase
-    git push
+    if git push ; then
+      echo '[+] push OK'
+    else
+      echo '[-] push failed'
+      let ret=1
+    fi
     popd > /dev/null
+    echo "ret is $ret"
+    return $ret
 }
 
 echo "---> Synchronizing from internet"
 
+let err=0
+let n=0
 
-ssync ~/.vim
-ssync ~/wiki/wiki
-ssync ~/wiki/wiki_html
-ssync ~/wiki/todo
-ssync ~/wiki/html
+let n++ ;    ssync ~/.vim           || let err++
+let n++ ;    ssync ~/wiki/wiki      || let err++
+let n++ &&    ssync ~/wiki/wiki_html || let err++
+let n++ &&    ssync ~/wiki/todo      || let err++
+let n++ &&    ssync ~/wiki/html      || let err++
 
 
-echo "<--- Synchronization finihed"
+echo "<--- Synchronization finihed with $err/$n failed"
