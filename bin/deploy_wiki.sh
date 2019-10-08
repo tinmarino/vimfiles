d749da40e86e884d9cbc3ba39fff02eec306952f
tinmarino <tinmarino@gmail.com>
2019-10-08 21:03:50 +0200

    Deploy_wiki.sh script to create wiki

---
+++ bin/deploy_wiki.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+function create_dir {
+  cd ~ && mkdir wiki \
+    && echo '[+] ~/wiki created' \
+    || echo '[-] ~/wiki not created already exists ?'
+  cd ~/wiki && mkdir wiki wiki_html todo todo_html html \
+    && echo '[+] all wiki dir created' \
+    || echo '[-] some wiki dir not created already exists ?'
+}
+
+function make_clone {
+  dir=$1
+  url=$2
+  echo "[*] Working with $dir"
+  [ -d $dir ] \
+    && cd $dir \
+    && git clone  $url . \
+    && echo "[+] $dir cloned from $url" \
+    || echo "[-] $dir not cloned maybe create the dir with function create_dir in this file"
+}
+
+create_dir
+make_clone ~/wiki/wiki https://gitlab.com/tinmarino/vimwiki.git
+make_clone ~/wiki/wiki_html https://gitlab.com/tinmarino/Wiki_Html.git  
+make_clone ~/wiki/todo https://gitlab.com/tinmarino/todo 
+make_clone ~/wiki/html https://gitlab.com/tinmarino/html.git
