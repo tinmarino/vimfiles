9428c52a4f14f9b4fad2f485b56d1294deed0e0d
tinmarino <tinmarino@gmail.com>
2019-11-25 02:36:45 -0300

    Bin set_gnomer shortcut to move windows to corner (in gnome 3)

---
+++ bin/set_gnome_shortcut.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+# gsettings set org.gnome.desktop.wm.keybindings move-to-corner-sw "['<Control><Alt>KP_1']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-side-s "['<Control><Alt>KP_2']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-corner-se "['<Control><Alt>KP_3']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "['<Control><Alt>KP_4']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-center "['<Control><Alt>KP_5']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "['<Control><Alt>KP_6']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-corner-nw "['<Control><Alt>KP_7']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-side-n "['<Control><Alt>KP_8']"
+# gsettings set org.gnome.desktop.wm.keybindings move-to-corner-ne "['<Control><Alt>KP_9']"
+
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-sw "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-s "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-se "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-center "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-nw "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-side-n "['disabled']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-corner-ne "['disabled']"
