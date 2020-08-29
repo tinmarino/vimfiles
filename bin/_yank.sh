22957501eb66b8b5cf9c8f7af1c583d3533b1c2b
Tinmarino <tinmarino@gmail.com>
2020-08-29 12:53:53 -0400

    Tmux clipboard working with OSC escape 52 (Operating System Control): \e]52;c;toto\a

--- bin/_yank.sh
+++ bin/_yank.sh
@@ -15,7 +15,11 @@ copy_use_osc52_fallback=$(tmux show-option -gvq "@copy_use_osc52_fallback")
 
 # Resolve copy backend: pbcopy (OSX), reattach-to-user-namespace (OSX), xclip/xsel (Linux)
 copy_backend=""
-if is_app_installed pbcopy; then
+if [ -n "${copy_backend_remote_tunnel_port-}" ] \
+    && (netstat -f inet -nl 2>/dev/null || netstat -4 -nl 2>/dev/null) \
+      | grep -q "[.:]$copy_backend_remote_tunnel_port"; then
+  copy_backend="nc localhost $copy_backend_remote_tunnel_port"
+elif is_app_installed pbcopy; then
   copy_backend="pbcopy"
 elif is_app_installed reattach-to-user-namespace; then
   copy_backend="reattach-to-user-namespace pbcopy"
@@ -23,11 +27,9 @@ elif [ -n "${DISPLAY-}" ] && is_app_installed xsel; then
   copy_backend="xsel -i --clipboard"
 elif [ -n "${DISPLAY-}" ] && is_app_installed xclip; then
   copy_backend="xclip -i -f -selection primary | xclip -i -selection clipboard"
-elif [ -n "${copy_backend_remote_tunnel_port-}" ] \
-    && (netstat -f inet -nl 2>/dev/null || netstat -4 -nl 2>/dev/null) \
-      | grep -q "[.:]$copy_backend_remote_tunnel_port"; then
-  copy_backend="nc localhost $copy_backend_remote_tunnel_port"
 fi
+#echo "Port $copy_backend_remote_tunnel_port"
+#echo "Backend $copy_backend"
 
 # if copy backend is resolved, copy and exit
 if [ -n "$copy_backend" ]; then
@@ -57,8 +59,8 @@ if [ "$buflen" -gt "$maxlen" ]; then
 fi
 
 # build up OSC 52 ANSI escape sequence
-esc="\033]52;c;$( printf %s "$buf" | head -c $maxlen | base64 | tr -d '\r\n' )\a"
-esc="\033Ptmux;\033$esc\033\\"
+esc="\e]52;c;$( printf %s "$buf" | head -c $maxlen | base64 | tr -d '\r\n' )\a"
+esc="\ePtmux;\e$esc\e\\"
 
 # resolve target terminal to send escape sequence
 # if we are on remote machine, send directly to SSH_TTY to transport escape sequence
