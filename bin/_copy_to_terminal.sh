61142e072572868a86f623b004eb46ab73726189
Tinmarino <tinmarino@gmail.com>
2021-01-14 23:31:04 -0300

    Vim improve Copy with OSC52 ,y

--- bin/_copy_to_terminal.sh
+++ bin/_copy_to_terminal.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 # Copy via Operating System Command 52 ANSI escape sequence to controlling terminal
-
+#
 # -- Used by Tmux
 # -- Ex: echo -e "\033]52;c;$(base64 <<< hellov0.1)\a"
 #
+# Source: https://github.com/sunaku/home/blob/master/bin/yank
 # More for vim: https://stackoverflow.com/questions/45247929/system-clipboard-vim-within-tmux-within-ssh-session
 # 
 
 # Get In <- data either form stdin or from file
 buf=$(cat "$@")
-echo Received "$buf" >> /tmp/copy
+echo Received "START>$buf<END" >> /tmp/tin_copy
 
 # Get len <- In buf
 buflen=$( printf %s "$buf" | wc -c )
@@ -22,18 +23,12 @@ maxlen=74994
 
 # Warn if exceeds maxlen
 if [ "$buflen" -gt "$maxlen" ]; then
-  printf "input is %d bytes too long" "$(( buflen - maxlen ))" >&2
+  printf "Warning: copy input is %d bytes too long" "$(( buflen - maxlen ))" >&2
 fi
 
 # Build up OSC 52 ANSI escape sequence
-esc="\e]52;c;$( printf %s "$buf" | head -c $maxlen | base64 | tr -d '\r\n' )\a"
+esc="\e]52;c;$( printf "%s" "$buf" | head -c $maxlen | base64 | tr -d '\r\n' )\a"
 esc="\ePtmux;\e$esc\e\\"
 
-# resolve target terminal to send escape sequence
-# if we are on remote machine, send directly to SSH_TTY to transport escape sequence
-# to terminal on local machine, so data lands in clipboard on our local machine
-#pane_active_tty=$(tmux list-panes -F "#{pane_active} #{pane_tty}" | awk '$1=="1" { print $2 }')
-#target_tty="${SSH_TTY:-$pane_active_tty}"
-
-printf "%s" "$esc"
-#printf "%s" "$esc" > /dev/tty
+# shellcheck disable=SC2059  #: Don't use variables 
+printf "$esc"
